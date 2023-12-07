c0f3c3b10f1be00509cfa229cdcbc9fe64752958
Amina <aliouaaminam@gmail.com>
2023-12-07 12:47:16 +0100

    suite

--- kmers.py
+++ kmers.py
@@ -16,5 +16,53 @@ def kmer2str(val, k):
 
 
 def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+    """
+    Input : text : from which we want the kmers
+            k : length of the kmer 
+    Output : kmer"
+    """
+    
+    # Dictionnary to encode and have the complementary kmers
+    encodage = {'A':0b00,'C':0b01,'T':0b10,'G':0b11}
+    complementary = {'A':'T','C':'G', 'T':'A', 'G':'C'}
+    
+    #Initialisation kmerb 
+    kmerb = 0b00
+    compl_kmerb = 0b00
+    
+    # Initialisation of a list that contain all the kmer found 
+    list_kmers = []
+    #print(f"IL ME SOULE CE TRUC 1 {type(kmerb)}")
+    # We get the k first values of the text 
+    for let in text[0:k]:
+        #print(f"IL ME SOULE CE TRUC 2 {type(kmerb)}")
+        kmerb = kmerb << 2
+        compl_kmerb <<= 2
+        #print(f"IL ME SOULE CE TRUC 3 {type(kmerb)}")
+        kmerb= kmerb | encodage[let]
+        compl_kmerb |= encodage[complementary[let]]
+        #print(f"IL ME SOULE CE TRUC 4 {type(kmerb)}")
+    
+    print(f"kmerb: {kmerb} et son complementary: {compl_kmerb}")
+    # Choose the minimum between 2 complementary kmer
+    min_kmerb = min(kmerb,compl_kmerb)
+    list_kmers.append(min_kmerb)
+    yield min_kmerb
+    
+    #Calcul mask
+    mask = (1<<(k*2))-1
+    
+    
+    for nucl in range(k,len(text)):
+        kmerb <<= 2
+        compl_kmerb <<= 2
+        kmerb |= encodage[text[nucl]]
+        compl_kmerb |= encodage[complementary[text[nucl]]]
+        kmerb &= mask
+        compl_kmerb &= mask
+        if min(kmerb,compl_kmerb) not in list_kmers:
+            yield min(kmerb,compl_kmerb)
+    
+    
+    
+
--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 from loading import load_directory
 from kmers import stream_kmers, kmer2str
+import numpy as np
 
 
 
 def jaccard(fileA, fileB, k):
-    j = 0
-    # --- To complete ---
-    return j
-
+     # dico kmers
+    dico_kmer = {}
+    intersection = 0
+    union = 0
+    
+    # Get the ones in seqA
+    for one_kmer in stream_kmers(fileA,k):
+        # Put in dico if not already in it
+        if one_kmer not in dico_kmer:
+            union += 1
+            dico_kmer[one_kmer] = 1
+        else:
+            union += 1
+            dico_kmer[one_kmer] +=1
+    for one_kmer in stream_kmers(fileB,k):
+        # Put in dico if not already in it
+        if one_kmer in dico_kmer:
+            intersection +=1
+            dico_kmer[one_kmer] -= 1
+            if dico_kmer[one_kmer] == 0:
+                del dico_kmer[one_kmer]
+        else:
+            union += 1   
+             
+    return intersection/union
 
 
 if __name__ == "__main__":
     # Load all the files in a dictionary
     files = load_directory("data")
-    k = 21
-    
+    k = 21 
+    dist_matrix = np.identity((len(files)))
+
     filenames = list(files.keys())
     for i in range(len(files)):
         for j in range(i+1, len(files)):
-            
-            # --- Complete here ---
+            coef = jaccard(files[filenames[i]],files[filenames[j]],k)
+            dist_matrix[i,j] = coef
+            #print(filenames[i], filenames[j], coef)
 
-            j = jaccard(files[filenames[i]], files[filenames[j]], k)
-            print(filenames[i], filenames[j], j)
+    print(dist_matrix)
